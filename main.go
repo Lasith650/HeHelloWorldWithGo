78728e9a7d15249c161722cee3c96a3bd4b7f4bb
lasith650 <lasith650@gmail.com>
2022-10-21 17:57:48 +0530

    Add getJson

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
@@ -32,6 +37,27 @@ func main() {
 		}
 		json.NewEncoder(rw).Encode(response)
 	})
+
+	router.HandleFunc("/getJson", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		user := &Person{}
+		err := json.NewDecoder(r.Body).Decode(user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		fmt.Println("got user:", user)
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != http.ErrServerClosed {
+		panic(err)
+	}
 
 	log.Println("Server is running!")
 	fmt.Println(http.ListenAndServe(":8081", router))
